c9f3a0f0c9352de72aa2b1dc5285ac57ebc2a564
Lucas Teixeira <lucasalvesteixeira@gmail.com>
2023-08-30 16:03:00 -0300

    adjusted the last commit

--- generics/GenericMethodTest.java
+++ generics/GenericMethodTest.java
@@ -10,7 +10,8 @@ public class GenericMethodTest { //metodo generico
         System.out.println();
     }
 
-    public static void main(String args[]) { // cria vetores dos tipos Integer, Double e Character
+    public static void main(String args[]) { 
+        // cria vetores dos tipos Integer, Double e Character
         Integer[] integerArray = { 1, 2, 3, 4, 5, 6 };
         Double[] doubleArray ={ 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
         Character[] characterArray = { 'H', 'E', 'L', 'L', 'O' };
